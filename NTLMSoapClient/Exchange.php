c264fa0981e448ba48b21ecdde1f044ffd0cfb1f
James I. Armes <jamesiarmes@gmail.com>
2009-06-16 03:33:28 +0000

    Fixed class and file names

---
+++ NTLMSoapClient/Exchange.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Handles Soap communication with the Exchnage server using NTLM
+ * authentication
+ * 
+ * @author James I. Armes <http://www.jamesarmes.net>
+ */
+class Exchange_NTLMSoapClient extends NTLMSoapClient {
+    protected $user;
+    protected $password;
+    
+    public function Exchange_NTLMSoapClient($wsdl, $options = array()) {
+		// verify that a user name and password were entered
+		if (empty($options['user']) || empty($options['password'])) {
+			throw new EWS_Exception('A username and password is required.');
+		} // end if no user name and password were entered
+
+		$this->user = $options['user'];
+		$this->password = $options['password'];
+
+		parent::SoapClient($wsdl, $options);
+    } // end function __contruct()
+} // end class Exchange_NTLMSoapClient
--- NTLMStream/Exchange.php
+++ NTLMStream/Exchange.php
@@ -1,5 +1,5 @@
 <?php
-class ExchangeNTLMStream extends NTLMStream {
+class Exchange_NTLMStream extends NTLMStream {
     protected $user = 'jarmes';
     protected $password = 'bobDude45';
 }
