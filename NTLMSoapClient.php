4630aa911a57c9a0a06b087697881b042bfff9b8
James I. Armes <jamesiarmes@gmail.com>
2009-08-24 18:48:32 +0000

    Fixed some comments Renamed constructor in NTLMSoapClient_Exchange to __construct()

--- NTLMSoapClient.php
+++ NTLMSoapClient.php
@@ -1,15 +1,15 @@
 <?php
 /**
  * Soap Client using Microsoft's NTLM Authentication
- * 
+ *
  * Copyright (c) 2008 Invest-In-France Agency http://www.invest-in-france.org
- * 
+ *
  * Author : Thomas Rabaix
- * 
+ *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
@@ -17,30 +17,30 @@
  * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- * 
+ *
  * @link http://rabaix.net/en/articles/2008/03/13/using-soap-php-with-ntlm-authentication
  * @author Thomas Rabaix
  */
 
 /**
  * Soap Client using Microsoft's NTLM Authentication
- * 
+ *
  * @link http://rabaix.net/en/articles/2008/03/13/using-soap-php-with-ntlm-authentication
  * @author Thomas Rabaix
  */
 class NTLMSoapClient extends SoapClient {
 	/**
 	 * Whether or not to validate ssl certificates
-	 * 
+	 *
 	 * @var boolean
 	 */
 	protected $validate = false;
-	
+
 	/**
 	 * Performs a SOAP request
-	 * 
+	 *
 	 * @link http://php.net/manual/en/function.soap-soapclient-dorequest.php
-	 * 
+	 *
 	 * @param string $request the xml soap request
 	 * @param string $location the url to request
 	 * @param string $action the soap action.
@@ -56,7 +56,7 @@ class NTLMSoapClient extends SoapClient {
 			'User-Agent: PHP-SOAP-CURL',
 			'Content-Type: text/xml; charset=utf-8',
 			'SOAPAction: "'.$action.'"',
-		);
+		); // end $headers
 		$this->__last_request_headers = $headers;
 		$ch = curl_init($location);
 
@@ -69,16 +69,16 @@ class NTLMSoapClient extends SoapClient {
 		curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
 		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_NTLM);
 		curl_setopt($ch, CURLOPT_USERPWD, $this->user.':'.$this->password);
-		
+
 		$response = curl_exec($ch);
 		return $response;
-	} // end function __doRequest)_
-	
+	} // end function __doRequest()
+
 	/**
 	 * Returns last SOAP request headers
-	 * 
+	 *
 	 * @link http://php.net/manual/en/function.soap-soapclient-getlastrequestheaders.php
-	 * 
+	 *
 	 * @return string the last soap request headers
 	 */
 	public function __getLastRequestHeaders() {
@@ -87,12 +87,12 @@ class NTLMSoapClient extends SoapClient {
 
 	/**
 	 * Sets whether or not to validate ssl certificates
-	 * 
+	 *
 	 * @param boolean $validate
 	 */
 	public function validateCertificate($validate = true) {
 		$this->validate = $validate;
-		
+
 		return true;
 	} // end public function validateCertificate()
 } // end class NTLMSoapClient
--- NTLMSoapClient/Exchange.php
+++ NTLMSoapClient/Exchange.php
@@ -3,21 +3,47 @@
  * Handles Soap communication with the Exchnage server using NTLM
  * authentication
  * 
- * @author James I. Armes <http://www.jamesarmes.net>
+ * @license http://www.opensource.org/licenses/bsd-license.php
+ * @author James I. Armes <jamesiarmes@gmail.com>
+ */
+
+/**
+ * Handles Soap communication with the Exchnage server using NTLM
+ * authentication
+ * 
+ * @author James I. Armes <jamesiarmes@gmail.com>
  */
 class NTLMSoapClient_Exchange extends NTLMSoapClient {
+	/**
+	 * Username for authentication on the exchnage server
+	 * 
+	 * @var string
+	 */
     protected $user;
+	
+	/**
+	 * password for authentication on the exchnage server
+	 * 
+	 * @var string
+	 */
     protected $password;
-    
-    public function Exchange_NTLMSoapClient($wsdl, $options = array()) {
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param string $wsdl
+	 * @param array $options
+	 */
+	public function __construct($wsdl, $options) {
 		// verify that a user name and password were entered
 		if (empty($options['user']) || empty($options['password'])) {
 			throw new EWS_Exception('A username and password is required.');
 		} // end if no user name and password were entered
-
+		
+		// set the username and password properties
 		$this->user = $options['user'];
 		$this->password = $options['password'];
-
-		parent::SoapClient($wsdl, $options);
-    } // end function __contruct()
+		
+		parent::__construct($wsdl, $options);
+	} // end function __construct()
 } // end class NTLMSoapClient_Exchange
