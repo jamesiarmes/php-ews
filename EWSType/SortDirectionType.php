4f196085f14ec2144fd227e0da8d4f8a9c37aff0
James I. Armes <jamesiarmes@gmail.com>
2009-08-25 11:52:53 +0000

    Added new type

---
+++ EWSType/SortDirectionType.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Definition of the SortDirectionType type
+ * 
+ * @author James I. Armes <http://www.jamesarmes.net>
+ */
+
+/**
+ * Sort order specification
+ * 
+ * @author James I. Armes <http://www.jamesarmes.net>
+ */
+class EWSType_SortDirectionType {
+	/**
+	 * Items are sorted in ascending order
+	 * 
+	 * @var string
+	 */
+	const ASCENDING = 'Ascending';
+	
+	/**
+	 * Items are sorted in descending order
+	 * 
+	 * @var string
+	 */
+	const DESCENDING = 'Descending';
+	
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+	} // end function __construct()
+} // end class EWSType_SortDirectionType
