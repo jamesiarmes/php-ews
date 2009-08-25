ee975fc38fc88ad6da38b3781f2a418f36ced71f
James I. Armes <jamesiarmes@gmail.com>
2009-08-25 11:35:56 +0000

    Added new type

---
+++ EWSType/ItemQueryTraversalType.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Definition of the ItemQueryTraversalType type
+ * 
+ * @author James I. Armes <http://www.jamesarmes.net>
+ */
+
+/**
+ * Types of sub-tree traversal for deletion and enumeration
+ * 
+ * @author James I. Armes <http://www.jamesarmes.net>
+ */
+class EWSType_ItemQueryTraversalType {
+	/**
+	 * Consider only folders that are direct children of the parent folder(s) in
+	 * question
+	 * 
+	 * @var string
+	 */
+	const SHALLOW = 'Shallow';
+	
+	/**
+	 * Consider only those items that are soft deleted from the parent folders
+	 * specified
+	 * 
+	 * @var string
+	 */
+	const SOFT_DELETED = 'SoftDeleted';
+	
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+	} // end function __construct()
+} // end class EWSType_ItemQueryTraversalType
