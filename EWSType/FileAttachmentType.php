c2f133907f6da8f8de450070b30d147593362a26
James I. Armes <jamesiarmes@gmail.com>
2012-06-12 17:27:10 -0400

    Added missing properties.

--- EWSType/FileAttachmentType.php
+++ EWSType/FileAttachmentType.php
@@ -1,27 +1,99 @@
 <?php
 /**
  * Definition of the FileAttachmentType type
- * 
+ *
  * @author James I. Armes <http://www.jamesarmes.net>
  */
 
 /**
  * Definition of the FileAttachmentType type
- * 
+ *
  * @author James I. Armes <http://www.jamesarmes.net>
  */
-class EWSType_FileAttachmentType extends EWSType {
+class EWSType_FileAttachmentType extends EWSType
+{
 	/**
-	 * Content property
-	 * 
-	 * @var EWSType_base64Binary
+	 * Identifies the file attachment.
+	 *
+	 * @var EWSType_AttachmentIdType
+	 */
+	public $AttachmentId;
+
+	/**
+	 * Represents the name of the attachment.
+	 *
+	 * @var string
+	 */
+	public $Name;
+
+	/**
+	 * Describes the Multipurpose Internet Mail Extensions (MIME) type of the
+	 * attachment content.
+	 *
+	 * @var string
+	 */
+	public $ContentType;
+
+	/**
+	 * Represents an identifier for the contents of an attachment. ContentId can
+	 * be set to any string value. Applications can use ContentId to implement
+	 * their own identification mechanisms.
+	 *
+	 * @var string
+	 */
+	public $ContentId;
+
+	/**
+	 * Contains the Uniform Resource Identifier (URI) that corresponds to the
+	 * location of the content of the attachment.
+	 *
+	 * @var string
+	 */
+	public $ContentLocation;
+
+	/**
+	 * Represents the size in bytes of the file attachment.
+	 *
+	 * @var integer
+	 */
+	public $Size;
+
+	/**
+	 * Represents when the file attachment was last modified.
+	 *
+	 * @var EWSType_dateTime
+	 */
+	public $LastModifiedTime;
+
+	/**
+	 * Represents whether the attachment appears inline within an item.
+	 *
+	 * @var boolean
+	 */
+	public $IsInline;
+	
+	/**
+	 * Indicates whether the file attachment is a contact picture.
+	 *
+	 * @var boolean
+	 */
+	public $IsContactPhoto;
+
+	/**
+	 * Contains the Base64-encoded contents of the file attachment.
+	 *
+	 * @var string
+	 *
+	 * @todo Consider using an object that takes a file stream and does the
+	 * base64 conversion.
 	 */
 	public $Content;
 
 	/**
 	 * Constructor
 	 */
-	public function __construct() {
+	public function __construct()
+	{
 		$this->schema = array(
 			array(
 				'name' => 'Content',
@@ -30,4 +102,4 @@ class EWSType_FileAttachmentType extends EWSType {
 			),
 		); // end $this->schema
 	} // end function __construct()
-} // end class FileAttachmentType
+} // end class EWSType_FileAttachmentType
