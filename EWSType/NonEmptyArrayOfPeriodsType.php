c46b52bcde64b69e33a3da523068787b5921209b
James I. Armes <jamesiarmes@gmail.com>
2012-10-10 22:01:46 -0400

    Issue #84 by @jamesiarmes: Added missing type definitions for timezones.

---
+++ EWSType/NonEmptyArrayOfPeriodsType.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The Period element defines the name, time offset, and unique identifier for a
+ * specific stage of the time zone.
+ *
+ * @package php-ews
+ * @subpackage Types
+ */
+
+/**
+ * Definition of the NonEmptyArrayOfPeriodsType type.
+ */
+class EWSType_NonEmptyArrayOfPeriodsType extends EWSType
+{
+    /**
+     * The name, time offset, and unique identifier for a specific stage of the
+     * time zone.
+     *
+     * @var EWSType_PeriodType
+     */
+    public $Period;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->schema = array(
+            array(
+                'name' => 'Id',
+                'required' => true,
+                'type' => 'array',
+            ),
+        );
+    }
+}
