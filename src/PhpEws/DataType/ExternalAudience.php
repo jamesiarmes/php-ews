160203c6015c514db23084b29e0f130f809ae702
kevint <kevint@commoncompanies.com>
2015-06-03 13:54:17 +0200

    Added ExternalAudience Used to define the external audience for UserOofSettings

---
+++ src/PhpEws/DataType/ExternalAudience.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace PhpEws\DataType;
+
+use PhpEws\DataType;
+
+/**
+ * Identifies to whom external Out of Office (OOF) messages are sent..
+ *
+ * @package php-ews\Enumerations
+ */
+class ExternalAudience extends DataType
+{
+    /**
+     * E-mail senders outside the mailbox user's organization who send messages
+     * to the user will receive an external OOF message response..
+     *
+     * @since Exchange 2007
+     *
+     * @var string
+     */
+    const ALL = 'All';
+
+    /**
+     * E-mail senders outside the mailbox user's organization who send messages
+     * to the user will only receive an external OOF message response if the
+     * sender is in the user's Exchange store contact list.
+     *
+     * @since Exchange 2007
+     *
+     * @var string
+     */
+    const KNOWN = 'Known';
+
+    /**
+     * E-mail senders outside the mailbox user's organization who send messages
+     * to the user will not receive an external OOF message response.
+     *
+     * @since Exchange 2007
+     *
+     * @var string
+     */
+    const NONE = 'None';
+
+    /**
+     * Element value.
+     *
+     * @var string
+     */
+    public $_;
+
+    /**
+     * Returns the value of this object as a string.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->_;
+    }
+}
