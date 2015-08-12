c9273ceee9158d5b7d00f43e750d2111850e7f2f
Garethp <blackfireweb@gmail.com>
2015-08-12 11:48:47 +0200

    Getting a list of emails

---
+++ src/Mail/MailAPI.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace jamesiarmes\PEWS\Mail;
+
+use jamesiarmes\PEWS\API;
+use jamesiarmes\PEWS\API\Type;
+use \DateTime;
+
+class MailAPI extends API
+{
+    protected $folderId;
+
+    public function getFolderId()
+    {
+        if (!$this->folderId) {
+            $this->folderId = $this->getFolderByDistinguishedId('inbox')->FolderId;
+        }
+
+        return $this->folderId;
+    }
+
+    public function getMailItems($options = array())
+    {
+        $request = array(
+            'Traversal' => 'Shallow',
+            'ItemShape' => array(
+                'BaseShape' => 'AllProperties'
+            ),
+            'ParentFolderIds' => array(
+                'FolderId' => array(
+                    'Id' => $this->getFolderId()->Id,
+                    'ChangeKey' => $this->getFolderId()->ChangeKey
+                )
+            )
+        );
+
+        $request = array_merge($request, $options);
+
+        $request = Type::buildFromArray($request);
+        $response = $this->getClient()->FindItem($request);
+        $items = $response;
+        if (!isset($items->Message)) {
+            return array();
+        }
+
+        if (!is_array($items->Message)) {
+            $items->Message = array($items->Message);
+        }
+
+        return $items->Message;
+    }
+}
