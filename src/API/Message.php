73c9ca4e901867f90289722e574777e10239a576
Garethp <blackfireweb@gmail.com>
2015-08-27 10:35:15 +0200

    Removing Duplicate code

--- src/API/Message.php
+++ src/API/Message.php
@@ -4,16 +4,4 @@ namespace jamesiarmes\PEWS\API;
 
 class Message extends Type
 {
-    public function getNonNullResponseMessages()
-    {
-        $messages = get_object_vars($this);
-
-        foreach ($messages as $key => $message) {
-            if (substr($key, 0, 1) == "_" || $message == null) {
-                unset($messages[$key]);
-            }
-        }
-
-        return $messages;
-    }
 }
