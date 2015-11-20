f40f2ea659500ce655ec51ad26e4eb7007a45bf3
Garethp <blackfireweb@gmail.com>
2015-11-20 14:03:21 +0100

    Add the ability to get a token from an authorization code

--- src/API/ExchangeWebServicesAuth.php
+++ src/API/ExchangeWebServicesAuth.php
@@ -2,6 +2,8 @@
 
 namespace jamesiarmes\PEWS\API;
 
+use GuzzleHttp\Client;
+
 class ExchangeWebServicesAuth
 {
     public static function fromUsernameAndPassword($username, $password)
@@ -21,5 +23,28 @@ class ExchangeWebServicesAuth
                 'Authorization' => 'Bearer ' . $token
             )
         );
+    }
+
+    public static function getTokenFromAuthorizationCode($clientId, $clientSecret, $authorizationCode, $redirectUri)
+    {
+        $postOptions = array(
+            'http_errors' => false,
+            'form_params' => array(
+                'client_id' => $clientId,
+                'client_secret' => $clientSecret,
+                'code' => $authorizationCode,
+                'redirect_uri' => $redirectUri,
+                'grant_type' => 'authorization_code'
+            )
+        );
+
+        $url = 'https://login.microsoftonline.com/common/oauth2/v2.0/token';
+
+        $client = new Client();
+        $response = $client->request('POST', $url, $postOptions);
+        $response = $response->getBody()->__toString();
+
+        $response = json_decode($response);
+        return $response->access_token;
     }
 }
