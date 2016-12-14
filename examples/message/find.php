37b8ceca6f0d34602cff144a974d3198af4c19b6
James Armes <james.armes@acquia.com>
2016-12-13 20:01:19 -0500

    Added message find example.

---
+++ examples/message/find.php
@@ -0,0 +1,89 @@
+<?php
+require_once '../../vendor/autoload.php';
+
+use \jamesiarmes\PhpEws\Client;
+use \jamesiarmes\PhpEws\Request\FindItemType;
+
+use \jamesiarmes\PhpEws\ArrayType\NonEmptyArrayOfBaseFolderIdsType;
+
+use \jamesiarmes\PhpEws\Enumeration\DefaultShapeNamesType;
+use \jamesiarmes\PhpEws\Enumeration\DistinguishedFolderIdNameType;
+use \jamesiarmes\PhpEws\Enumeration\UnindexedFieldURIType;
+use \jamesiarmes\PhpEws\Enumeration\ResponseClassType;
+
+use \jamesiarmes\PhpEws\Type\ConstantValueType;
+use \jamesiarmes\PhpEws\Type\DistinguishedFolderIdType;
+use \jamesiarmes\PhpEws\Type\FieldURIOrConstantType;
+use \jamesiarmes\PhpEws\Type\IsGreaterThanOrEqualToType;
+use \jamesiarmes\PhpEws\Type\IsLessThanOrEqualToType;
+use \jamesiarmes\PhpEws\Type\ItemResponseShapeType;
+use \jamesiarmes\PhpEws\Type\PathToUnindexedFieldType;
+
+// Replace with the date range you want to search in. As is, this will find all
+// messages within the current calendar year.
+$start_date = new DateTime('January 1 00:00:00');
+$end_date = new DateTime('December 31 23:59:59');
+$timezone = 'Eastern Standard Time';
+
+// Set connection information.
+$host = '';
+$username = '';
+$password = '';
+$version = Client::VERSION_2016;
+
+$client = new Client($host, $username, $password, $version);
+$client->setTimezone($timezone);
+
+$request = new FindItemType();
+$request->ParentFolderIds = new NonEmptyArrayOfBaseFolderIdsType();
+
+// Build the start date restriction.
+$greater_than = new IsGreaterThanOrEqualToType();
+$greater_than->FieldURI = new PathToUnindexedFieldType();
+$greater_than->FieldURI->FieldURI = UnindexedFieldURIType::ITEM_DATE_TIME_RECEIVED;
+$greater_than->FieldURIOrConstant = new FieldURIOrConstantType();
+$greater_than->FieldURIOrConstant->Constant = new ConstantValueType();
+$greater_than->FieldURIOrConstant->Constant->Value = $start_date->format('c');
+
+// Build the end date restriction;
+$less_than = new IsLessThanOrEqualToType();
+$less_than->FieldURI = new PathToUnindexedFieldType();
+$less_than->FieldURI->FieldURI = UnindexedFieldURIType::ITEM_DATE_TIME_RECEIVED;
+$less_than->FieldURIOrConstant = new FieldURIOrConstantType();
+$less_than->FieldURIOrConstant->Constant = new ConstantValueType();
+$less_than->FieldURIOrConstant->Constant->Value = $end_date->format('c');
+
+// Build the restriction.
+$request->Restriction = new \jamesiarmes\PhpEws\Type\RestrictionType();
+$request->Restriction->And = new \jamesiarmes\PhpEws\Type\AndType();
+$request->Restriction->And->IsGreaterThanOrEqualTo = $greater_than;
+$request->Restriction->And->IsLessThanOrEqualTo = $less_than;
+
+// Return all event properties.
+$request->ItemShape = new ItemResponseShapeType();
+$request->ItemShape->BaseShape = DefaultShapeNamesType::ALL_PROPERTIES;
+
+// Search in the user's inbox.
+$folder_id = new DistinguishedFolderIdType();
+$folder_id->Id = DistinguishedFolderIdNameType::INBOX;
+$request->ParentFolderIds->DistinguishedFolderId[] = $folder_id;
+
+$response = $client->FindItem($request);
+
+// Iterate over the results, printing any error messages or message subjects.
+$response_messages = $response->ResponseMessages->FindItemResponseMessage;
+foreach ($response_messages as $response_message) {
+    // Make sure the request succeeded.
+    if ($response_message->ResponseClass != ResponseClassType::SUCCESS) {
+        $message = $response_message->ResponseCode;
+        fwrite(STDERR, "Failed to search for events with \"$message\"\n");
+        continue;
+    }
+
+    // Iterate over the messages that were found, printing the subject for each.
+    $items = $response_message->RootFolder->Items->Message;
+    foreach ($items as $item) {
+        $subject = $item->Subject;
+        fwrite(STDOUT, "$subject\n");
+    }
+}
