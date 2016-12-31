c672322539ccbd04811bec3a3e8b717e85b616eb
James Armes <james.armes@acquia.com>
2016-12-30 23:32:00 -0500

    Added autodiscover options.

---
+++ examples/autodiscover/basic.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This example shows how to use the Autodiscover to attempt to discover the
+ * correct host URL and settings for a Microsoft Exchange server. If
+ * autodiscovery fails, you will need to determine the correct URL on your own
+ * and instantiate the Client object directly.
+ *
+ * Autodiscovery can take several requests. For this reason, it is recommended
+ * that you cache the host name and server version for future use, rather than
+ * using the service for each request.
+ *
+ * For more information on autodiscovery, see
+ * https://technet.microsoft.com/en-us/library/bb124251(v=exchg.150).aspx.
+ */
+require_once '../../vendor/autoload.php';
+
+use jamesiarmes\PhpEws\Autodiscover;
+
+// Replace these with the appropriate values.
+$email = '';
+$password = '';
+
+// Simplest usage, no special options.
+$client = Autodiscover::getEWS($email, $password);
+
+// If autodiscovery failed, throw an exception.
+if (!$client) {
+    throw new Exception('Autodiscovery failed.');
+}
---
+++ examples/autodiscover/certificate-authority.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This example shows how to use the Autodiscover with a custom certificate
+ * authority. See the basic example for more information on autodiscovery.
+ */
+require_once '../../vendor/autoload.php';
+
+use jamesiarmes\PhpEws\Autodiscover;
+
+// Replace these with the appropriate values.
+$email = '';
+$password = '';
+$ca_path = '/path/to/your/cacert.pem';
+
+// Set the custom CA then attemtpy autodiscovery.
+$discovery = new Autodiscover($email, $password);
+$discovery->setCAInfo($ca_path);
+$client = $discovery->newEWS();
+
+// If autodiscovery failed, throw an exception.
+if (!$client) {
+    throw new Exception('Autodiscovery failed.');
+}
