2b2b4a011fb53cc48ecade727fdb5bf202142c6e
James Armes <james.armes@acquia.com>
2017-01-01 17:41:37 -0500

    Added an extended property to the event/describe example.

--- examples/event/describe.php
+++ examples/event/describe.php
@@ -4,13 +4,17 @@ require_once '../../vendor/autoload.php';
 use \jamesiarmes\PhpEws\Client;
 use \jamesiarmes\PhpEws\Request\GetItemType;
 
+use \jamesiarmes\PhpEws\ArrayType\NonEmptyArrayOfPathsToElementType;
 use \jamesiarmes\PhpEws\ArrayType\NonEmptyArrayOfBaseItemIdsType;
 
 use \jamesiarmes\PhpEws\Enumeration\DefaultShapeNamesType;
+use \jamesiarmes\PhpEws\Enumeration\DistinguishedPropertySetType;
+use \jamesiarmes\PhpEws\Enumeration\MapiPropertyTypeType;
 use \jamesiarmes\PhpEws\Enumeration\ResponseClassType;
 
 use \jamesiarmes\PhpEws\Type\ItemIdType;
 use \jamesiarmes\PhpEws\Type\ItemResponseShapeType;
+use \jamesiarmes\PhpEws\Type\PathToExtendedFieldType;
 
 // Replace this with the ids of actual events.
 $event_ids = array(
@@ -35,6 +39,17 @@ $request = new GetItemType();
 $request->ItemShape = new ItemResponseShapeType();
 $request->ItemShape->BaseShape = DefaultShapeNamesType::ALL_PROPERTIES;
 $request->ItemIds = new NonEmptyArrayOfBaseItemIdsType();
+
+// We want to get the online meeting link in the response. Note that if this
+// property is not set on the event, it will not be included in the response.
+$property = new PathToExtendedFieldType();
+$property->PropertyName = 'OnlineMeetingExternalLink';
+$property->PropertyType = MapiPropertyTypeType::STRING;
+$property->DistinguishedPropertySetId = DistinguishedPropertySetType::PUBLIC_STRINGS;
+
+$additional_properties = new NonEmptyArrayOfPathsToElementType();
+$additional_properties->ExtendedFieldURI[] = $property;
+$request->ItemShape->AdditionalProperties = $additional_properties;
 
 // Iterate over the event ids, setting each one on the request.
 foreach ($event_ids as $event_id) {
